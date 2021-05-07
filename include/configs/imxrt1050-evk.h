435d78cb5208fd98e7cfef1c1d142d136fd8ffe2
Giulio Benetti <giulio.benetti@benettiengineering.com>
2021-05-07 20:33:28 +0200

    SQ board: freescale: imxrt1050-evk: remove hard-coded FEC defines
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

--- include/configs/imxrt1050-evk.h
+++ include/configs/imxrt1050-evk.h
@@ -25,10 +25,5 @@
 		"stderr=serial,vidconsole\0"
 #endif
 
-/* ENET */
-#define CONFIG_ETHPRIME			"FEC"
-#define CONFIG_FEC_XCV_TYPE		RMII
-#define CONFIG_FEC_MXC_PHYADDR		2
-#define IMX_FEC_BASE			0x402D8000
 
 #endif /* __IMXRT1050_EVK_H */
