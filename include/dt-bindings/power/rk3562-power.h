225d13d96f030248c8ab68ddc4500ad5ad4ac2ad
Giulio Benetti <giulio.benetti@benettiengineering.com>
2024-02-10 21:24:21 +0100

    include/dt-bindings/power/rk3562-power.h: add missing file to create dtb
    Signed-off-by: Giulio Benetti <giulio.benetti@benettiengineering.com>

---
+++ include/dt-bindings/power/rk3562-power.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __DT_BINDINGS_POWER_RK3562_POWER_H__
+#define __DT_BINDINGS_POWER_RK3562_POWER_H__
+
+/* VD_CORE */
+#define RK3562_PD_CPU_0		0
+#define RK3562_PD_CPU_1		1
+#define RK3562_PD_CPU_2		2
+#define RK3562_PD_CPU_3		3
+#define RK3562_PD_CORE_ALIVE	4
+
+/* VD_PMU */
+#define RK3562_PD_PMU		5
+
+/* VD_NPU */
+#define RK3562_PD_NPU		6
+
+/* VD_GPU */
+#define RK3562_PD_GPU		7
+
+/* VD_LOGIC */
+#define RK3562_PD_VI		8
+#define RK3562_PD_VO		9
+#define RK3562_PD_RGA		10
+#define RK3562_PD_VPU		11
+#define RK3562_PD_CENTER	12
+#define RK3562_PD_RKVDEC	13
+#define RK3562_PD_RKVENC	14
+#define RK3562_PD_PIPE		15
+#define RK3562_PD_LOGIC_ALIVE	16
+
+#endif
